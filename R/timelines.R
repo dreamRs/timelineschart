1b959867bb4c95ebc53a85d13ad70a452797066b
pvictor <perrier.victor@gmail.com>
2019-01-29 18:20:39 +0100

    parse data & doc

--- R/timelines.R
+++ R/timelines.R
@@ -1,11 +1,37 @@
-#' <Add Title>
+#' Timelines chart
 #'
-#' <Add Description>
+#' @param data A \code{data.frame} (see details) or a list.
+#' @param line_height Maximum height of each line, in px.
+#' @param width,height Width and height for the widget.
+#' @param element_id A custom id for the widget.
+#'
+#' @details Data format
+#'
+#' If a \code{data.frame} is used, it must contains the following columns:
+#'
+#' \describe{
+#'   \item{group}{Name of the group, used in left-side of the timelines chart.}
+#'   \item{label}{Label for the line.}
+#'   \item{start}{Start of the event.}
+#'   \item{end}{End of the event.}
+#'   \item{value}{Value to be represented, can be continuous or discrete.}
+#'  }
 #'
 #' @importFrom htmlwidgets createWidget sizingPolicy
 #'
 #' @export
 timelines <- function(data, line_height = 12, width = NULL, height = NULL, element_id = NULL) {
+
+  is_discrete <- FALSE
+  domain <- c(0, 1)
+  if (is.data.frame(data)) {
+    if (!all(c("group", "label", "start", "end", "value") %in% names(data)))
+      stop("data must contains columns: group, label, start, end, value", call. = FALSE)
+    is_discrete <- !is.numeric(data$value)
+    domain <- range(pretty(range(data$value, na.rm = TRUE)))
+    data <- parse_data(data)
+
+  }
 
   config = list(
     data = data,
@@ -19,8 +45,8 @@ timelines <- function(data, line_height = 12, width = NULL, height = NULL, eleme
       ),
       scale = list(
         palette = "Viridis",
-        domain = c(0, 1),
-        discrete = FALSE,
+        domain = domain,
+        discrete = is_discrete,
         data_label = "",
         label = ""
       ),
--- R/utils.R
+++ R/utils.R
@@ -44,3 +44,37 @@
 
   return(timelines)
 }
+
+
+# Parse data to timelines-chart format
+parse_data <- function(data) {
+  lapply(
+    X = unique(data$group),
+    FUN = function(x) {
+      dat_group <- data[data$group %in% x, ]
+      list(
+        group = x,
+        data = lapply(
+          X = unique(dat_group$label),
+          FUN = function(y) {
+            dat_label <- dat_group[dat_group$label %in% y, ]
+            list(
+              label = y,
+              data = lapply(
+                X = seq_along(nrow(dat_label)),
+                FUN = function(z) {
+                  list(
+                    timeRange = c(dat_label$start[z], dat_label$end[z]),
+                    val = dat_label$value[z]
+                  )
+                }
+              )
+            )
+          }
+        )
+      )
+    }
+  )
+}
+
+
